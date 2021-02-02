75f3dd521693c2d7a8ca7bb612a45878f861fda9
kseniya <efremovaks@gmail.com>
2021-02-02 17:34:36 +0300

    Нужно больше функций - убрала лишнее

--- js/main.js
+++ js/main.js
@@ -1,26 +1,31 @@
-function random(min, max) {
-  if (min < 0 || max < 0 || min > max || min === max) {
-    return null
-  } else {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+function random(min = 0, max = 10) {
+
+  if (min < 0 || max < 0 || min > max) {
+    return null;
   }
+
+  if (min === max) {
+    return min;
+  }
+
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-console.log('целое число из диапазона(min > max): ' + random(5, 1));
-console.log('целое число из диапазона(minus): ' + random(-5, -1));
-console.log('целое число из диапазона(min < max): ' + random(0, 8));
+random()
 
 
+function randomFloat(min = 0, max = 10) {
 
-function randomFloat (min, max) {
-  if (min < 0 || max < 0 || min > max || min === max) {
-    return null
-  } else {
-    let rand =  Math.random() * (max - min + 1) + min;
-    return rand.toFixed(1)
+  if (min < 0 || max < 0 || min > max) {
+    return null;
   }
+
+  if (min === max) {
+    return min;
+  }
+
+  let rand = Math.random() * (max - min + 1) + min;
+  return rand.toFixed(1);
 }
 
-console.log('число с плавающей точкой из диапазона (min > max): ' + randomFloat(10, 5));
-console.log('число с плавающей точкой из диапазона (min < max): ' + randomFloat(1, 5));
-console.log('число с плавающей точкой из диапазона (minus): ' + randomFloat(1, 1));
+randomFloat()
