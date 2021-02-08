018d60304b114a4485904968502b6e73a9c701bf
kseniya <efremovaks@gmail.com>
2021-02-08 11:17:54 +0300

    Больше деталей - массивы

--- js/main.js
+++ js/main.js
@@ -32,35 +32,53 @@ randomFloat();
 
 
 // описывает автора
-
-let author = { // let или const ?
-  avatar : 'img/avatars/user{{xx}}.png', // вместо {{xx}} , судя по всему должна вызываться функция, которая генерит число, вопрос - как ее вкорячить в адрес?
-};
+// const avatars = ['img/avatars/user{{xx}}.png'];
 
 
 // содержит информацию об объявлении
 
-let offerType = ['palace', 'flat', 'house', 'bungalow'];
-let offerFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-let offerPhotos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg']
+const title = ['заголовок предложения-1', 'заголовок предложения-2', 'заголовок предложения-3'];
+const address = [];
+const type = ['palace', 'flat', 'house', 'bungalow'];
+const checkin = ['12:00', '13:00', '14:00'];
+const checkout = ['12:00', '13:00', '14:00'];
+const features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const description = ['описание помещения-1', 'описание помещения-2', 'описание помещения-3']
+const photos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
-let offer = {
-  title : 'заголовок предложения-1',
-  address : '{{location.x}}, {{location.y}}',
-  price : 1, // тут тоже берется рандомное число из функции?
-  type : offerType[i], //что-то из масива?
-  rooms : 1, // рандомное число из функции?
-  guests : 1, // рандомное число из функции?
-  checkin : '12:00', // 13:00 или 14:00
-  checkout : '12:00', // 13:00 или 14:00
-  features : 'wifi', //что-то из масива?
-  description : 'описание помещения-1',
-  photos : 'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-};
 
 // местоположение в виде географических координат
 
-let location = {
-  x : 35.65000, //широта. случайное значение от 35.65000 до 35.70000
-  y : 139.70000, //долгота. случайное значение от 139.70000 до 139.80000
+// const location = {
+//   x: 35.65000, //широта. случайное значение от 35.65000 до 35.70000
+//   y: 139.70000, //долгота. случайное значение от 139.70000 до 139.80000
+// }
+
+
+function total() {
+
+  return {
+    author {
+      avatars: '',
+    };
+
+    offer {
+      title: '',
+      address: '',
+      price: '',
+      type: '',
+      rooms: '',
+      guests: '',
+      checkin: '',
+      checkout: '',
+      features: '',
+      description: '',
+      photos: '',
+    };
+
+    location {
+      x: '',
+      y: '',
+    }
+  }
 }
