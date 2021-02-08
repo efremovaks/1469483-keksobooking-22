43caa3a5d0da6c4a6ac961c8489b36652bb1ec8d
kseniya <efremovaks@gmail.com>
2021-02-08 13:24:27 +0300

    Больше деталей - генерим данные

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function random(min = 0, max = 10) {
+function random(min, max) {
 
   if (min < 0 || max < 0 || min > max) {
     return null;
@@ -11,7 +11,6 @@ function random(min = 0, max = 10) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-random();
 
 
 function randomFloat(min = 0, max = 10) {
@@ -28,57 +27,53 @@ function randomFloat(min = 0, max = 10) {
   return rand.toFixed(1);
 }
 
-randomFloat();
 
-
-// описывает автора
-// const avatars = ['img/avatars/user{{xx}}.png'];
-
-
-// содержит информацию об объявлении
 
 const title = ['заголовок предложения-1', 'заголовок предложения-2', 'заголовок предложения-3'];
-const address = [];
 const type = ['palace', 'flat', 'house', 'bungalow'];
-const checkin = ['12:00', '13:00', '14:00'];
-const checkout = ['12:00', '13:00', '14:00'];
+const checkinCheckout = ['12:00', '13:00', '14:00'];
 const features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-const description = ['описание помещения-1', 'описание помещения-2', 'описание помещения-3']
+const description = ['описание помещения-1', 'описание помещения-2', 'описание помещения-3'];
 const photos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
 
-// местоположение в виде географических координат
-
-// const location = {
-//   x: 35.65000, //широта. случайное значение от 35.65000 до 35.70000
-//   y: 139.70000, //долгота. случайное значение от 139.70000 до 139.80000
-// }
 
 
-function total() {
+function setOffer() {
+  let randomAvatar = random(1, 8);
 
   return {
-    author {
-      avatars: '',
-    };
+    author: {
+      avatar: 'img/avatars/user' + '0' + randomAvatar + '.png',
+    },
 
-    offer {
+    offer: {
       title: '',
       address: '',
-      price: '',
+      price: 0,
       type: '',
-      rooms: '',
-      guests: '',
+      rooms: 0,
+      guests: 0,
       checkin: '',
       checkout: '',
       features: '',
       description: '',
       photos: '',
-    };
+    },
 
-    location {
-      x: '',
-      y: '',
-    }
-  }
+    location: {
+      x: 0,
+      y: 0,
+    },
+  };
 }
+
+function createCollection() {
+  let collection = [];
+  for (let i = 0; i < 10; i++) {
+    collection.push(setOffer());
+  }
+  return collection;
+}
+
+console.log(createCollection());
