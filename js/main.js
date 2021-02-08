0496c8a65e121156a4fcdb5f70cf3e24f11528c5
kseniya <efremovaks@gmail.com>
2021-02-08 14:26:14 +0300

    Больше деталей - заканчиваем генерить данные

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function random(min, max) {
 
   if (min < 0 || max < 0 || min > max) {
-    return null;
+    return 0;
   }
 
   if (min === max) {
@@ -16,7 +16,7 @@ function random(min, max) {
 function randomFloat(min = 0, max = 10) {
 
   if (min < 0 || max < 0 || min > max) {
-    return null;
+    return 0;
   }
 
   if (min === max) {
@@ -24,7 +24,7 @@ function randomFloat(min = 0, max = 10) {
   }
 
   let rand = Math.random() * (max - min + 1) + min;
-  return rand.toFixed(1);
+  return rand.toFixed(5);
 }
 
 
@@ -38,32 +38,78 @@ const photos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0
 
 
 
+function getAvatar() {
+  let index = random(1, 8);
+
+  return 'img/avatars/user' + '0' + index + '.png';
+}
+
+
+function getTitle() {
+  let titleName = random(0, title.length - 1);
+
+  return title[titleName];
+}
+
+
+// function getPrice() {
+//   let randomPrice = random(100, 10000);
+
+//   return randomPrice;
+// }
+
+
+function getType() {
+  let i = random(0, type.length - 1);
+
+  return type[i];
+}
+
+
+function getCheckin() {
+  let i = random(0, checkinCheckout.length - 1);
+
+  return checkinCheckout[i];
+}
+
+
+function getCheckout() {
+  let i = random(0, checkinCheckout.length - 1);
+
+  return checkinCheckout[i];
+}
+
+
+function getDescription() {
+  let i = random(0, description.length - 1);
+
+  return description[i];
+}
 
 function setOffer() {
-  let randomAvatar = random(1, 8);
 
   return {
     author: {
-      avatar: 'img/avatars/user' + '0' + randomAvatar + '.png',
+      avatar: getAvatar(),
     },
 
     offer: {
-      title: '',
-      address: '',
-      price: 0,
-      type: '',
-      rooms: 0,
-      guests: 0,
-      checkin: '',
-      checkout: '',
+      title: getTitle(),
+      address: randomFloat(35.65000, 35.70000) + ', ' + randomFloat(139.70000, 139.80000),
+      price: random(100, 10000), //getPrice()
+      type: getType(),
+      rooms: random(1, 10),
+      guests: random(1, 30),
+      checkin: getCheckin(),
+      checkout: getCheckout(),
       features: '',
-      description: '',
+      description: getDescription(),
       photos: '',
     },
 
     location: {
-      x: 0,
-      y: 0,
+      x: randomFloat(35.65000, 35.70000),
+      y: randomFloat(139.70000, 139.80000),
     },
   };
 }
