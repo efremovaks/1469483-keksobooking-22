e253e6078d832f4b69c468865db3a918a6d339c2
kseniya <efremovaks@gmail.com>
2021-02-17 12:54:33 +0300

    Отрисуй меня полностью (часть 1)

--- js/cards.js
+++ js/cards.js
@@ -4,18 +4,20 @@ const template = document.querySelector('#card').content.querySelector('.popup')
 const mapCanvas = document.querySelector('.map-canvas');
 
 function card () {
+  const cardTemplate = template.cloneNode(true);
 
-  const title = template.querySelector('.popup__title').textContent = offer.title;
-  const address = template.querySelector('.popup__text--address').textContent = offer.address;
-  const price = template.querySelector('.popup__text--price').textContent = offer.price + '₽/ночь';
-  const type = template.querySelector('.popup__type').textContent = offer.type; // словари
-  const capacity = template.querySelector('.popup__text--capacity').textContent = offer.rooms + 'комнаты для' + offer.guests + 'гостей';
-  const time = template.querySelector('.popup__text--time').textContent = 'Заезд после' + offer.checkin + 'выезд до' + offer.checkout;
-  const features = template.querySelector('.popup__features').textContent = offer.features;
-  const description = template.querySelector('.popup__description').textContent = offer.description;
-  const photos = template.querySelector('.popup__photo').src = offer.photos;
-  const avatar = template.querySelector('.popup__avatar').src = offer.avatar;
+  const title = cardTemplate.querySelector('.popup__title').textContent = offer.title;
+  const address = cardTemplate.querySelector('.popup__text--address').textContent = offer.address;
+  const price = cardTemplate.querySelector('.popup__text--price').textContent = offer.price + '₽/ночь';
+  const type = cardTemplate.querySelector('.popup__type').textContent = offer.type; // словари
+  const capacity = cardTemplate.querySelector('.popup__text--capacity').textContent = offer.rooms + ' комнаты для ' + offer.guests + ' гостей';
+  const time = cardTemplate.querySelector('.popup__text--time').textContent = 'Заезд после ' + offer.checkin + ' выезд до ' + offer.checkout;
+  const features = cardTemplate.querySelector('.popup__features').textContent = offer.features;
+  const description = cardTemplate.querySelector('.popup__description').textContent = offer.description;
+  const photos = cardTemplate.querySelector('.popup__photo').src = offer.photos;
+  const avatar = cardTemplate.querySelector('.popup__avatar').src = offer.avatar;
 
+  return cardTemplate
 }
 
-card()
+console.log(card())
