3ec900ae5489444e100f462210e6a43aba41c858
kseniya <efremovaks@gmail.com>
2021-02-18 21:07:56 +0300

    Отрисуй меня полностью (часть 2)

---
+++ js/form.js
@@ -0,0 +1,15 @@
+const typeMinPrice = {
+  bungalow: 0,
+  flat: 1000,
+  house: 5000,
+  palace: 10000,
+};
+
+const form = document.querySelector('.ad-form');
+const priceFormInput = form.querySelector('#price');
+const typeForm = form.querySelector('#type');
+const timein = form.querySelector('#timein');
+const timeout = form.querySelector('#timeout');
+
+
+
