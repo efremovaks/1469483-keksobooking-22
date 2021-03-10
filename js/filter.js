2357c10bc943ef179065293a9b75fb0d23a464ec
kseniya <efremovaks@gmail.com>
2021-03-10 13:49:22 +0300

    Перламутровые пуговицы (часть 2) - передел

--- js/filter.js
+++ js/filter.js
@@ -19,6 +19,7 @@ const PriceRange = {
 
 
 const mapForm = document.querySelector('.map__filters');
+const selects = mapForm.querySelectorAll('select');
 const houseType = mapForm.querySelector('#housing-type');
 const housingPrice = mapForm.querySelector('#housing-price');
 const housingRooms = mapForm.querySelector('#housing-rooms');
@@ -26,63 +27,87 @@ const housingGuests = mapForm.querySelector('#housing-guests');
 
 
 function addFilterListener(offers) {
+  mapForm.addEventListener('change', function () {
+    let filteredOffers = [];
 
-  houseType.addEventListener('change', function () {
+    selects.forEach((select) => {
 
-    // создаем коллекцию подходящих элементов
-    if (houseType.value === 'any') {
-      reRenderMarkers(offers);
-      return
-    }
-    const filteredOffers = offers.filter((item) => item.offer.type === houseType.value);
+      const selectPropType = select.name.split('-')[1];
+
+      if (select.value === 'any') {
+        reRenderMarkers(offers);
+      }
+
+      const newOffers = offers.filter((item) => item.offer[selectPropType] === select.value);
+      console.log(newOffers)
+
+      filteredOffers.push(newOffers);
+    })
+
     reRenderMarkers(filteredOffers);
   });
-
-  housingPrice.addEventListener('change', function () {
-    if (housingPrice.value === 'low') {
-      const filteredPrice = offers.filter((item) => item.offer.price <= PriceRange.LOW.MAX);
-      reRenderMarkers(filteredPrice);
-    } else if (housingPrice.value === 'middle') {
-      const filteredPrice = offers.filter((item) => item.offer.price > PriceRange.MIDDLE.MIN && item.offer.price < PriceRange.MIDDLE.MAX);
-      reRenderMarkers(filteredPrice);
-    } else if ((housingPrice.value === 'high')) {
-      const filteredPrice = offers.filter((item) => item.offer.price > PriceRange.HIGH.MIN);
-      reRenderMarkers(filteredPrice);
-    } else {
-      reRenderMarkers(offers);
-    }
-  });
-
-  housingRooms.addEventListener('change', function () {
-    if (+housingRooms.value === 1) {
-      const filteredRooms = offers.filter((item) => item.offer.rooms === 1);
-      reRenderMarkers(filteredRooms)
-    } else if (+housingRooms.value === 2) {
-      const filteredRooms = offers.filter((item) => item.offer.rooms === 2);
-      reRenderMarkers(filteredRooms)
-    } else if (+housingRooms.value === 3) {
-      const filteredRooms = offers.filter((item) => item.offer.rooms === 3);
-      reRenderMarkers(filteredRooms)
-    } else {
-      reRenderMarkers(offers);
-    }
-  });
-
-  housingGuests.addEventListener('change', function () {
-    if (+housingGuests.value === 0) {
-      const filteredGuests = offers.filter((item) => item.offer.guests === 0);
-      reRenderMarkers(filteredGuests);
-    } else if (+housingGuests.value === 1) {
-      const filteredGuests = offers.filter((item) => item.offer.guests === 1);
-      reRenderMarkers(filteredGuests);
-    } else if (+housingGuests.value === 2) {
-      const filteredGuests = offers.filter((item) => item.offer.guests === 2);
-      reRenderMarkers(filteredGuests);
-    } else {
-      reRenderMarkers(offers);
-    }
-  })
 }
+
+
+
+// function addFilterListener(offers) {
+
+//   houseType.addEventListener('change', function () {
+//     if (houseType.value === 'any') {
+//       reRenderMarkers(offers);
+//       return
+//     }
+//     const filteredOffers = offers.filter((item) => item.offer.type === houseType.value);
+//     reRenderMarkers(filteredOffers);
+//   });
+
+//   housingPrice.addEventListener('change', function () {
+//     if (housingPrice.value === 'low') {
+//       const filteredPrice = offers.filter((item) => item.offer.price <= PriceRange.LOW.MAX);
+//       reRenderMarkers(filteredPrice);
+//     } else if (housingPrice.value === 'middle') {
+//       const filteredPrice = offers.filter((item) => item.offer.price > PriceRange.MIDDLE.MIN && item.offer.price < PriceRange.MIDDLE.MAX);
+//       reRenderMarkers(filteredPrice);
+//     } else if ((housingPrice.value === 'high')) {
+//       const filteredPrice = offers.filter((item) => item.offer.price > PriceRange.HIGH.MIN);
+//       reRenderMarkers(filteredPrice);
+//     } else {
+//       reRenderMarkers(offers);
+//     }
+//   });
+
+//   housingRooms.addEventListener('change', function () {
+//     if (+housingRooms.value === 1) {
+//       const filteredRooms = offers.filter((item) => item.offer.rooms === 1);
+//       reRenderMarkers(filteredRooms)
+//     } else if (+housingRooms.value === 2) {
+//       const filteredRooms = offers.filter((item) => item.offer.rooms === 2);
+//       reRenderMarkers(filteredRooms)
+//     } else if (+housingRooms.value === 3) {
+//       const filteredRooms = offers.filter((item) => item.offer.rooms === 3);
+//       reRenderMarkers(filteredRooms)
+//     } else {
+//       reRenderMarkers(offers);
+//     }
+//   });
+
+//   housingGuests.addEventListener('change', function () {
+//     if (+housingGuests.value === 0) {
+//       const filteredGuests = offers.filter((item) => item.offer.guests === 0);
+//       reRenderMarkers(filteredGuests);
+//     } else if (+housingGuests.value === 1) {
+//       const filteredGuests = offers.filter((item) => item.offer.guests === 1);
+//       reRenderMarkers(filteredGuests);
+//     } else if (+housingGuests.value === 2) {
+//       const filteredGuests = offers.filter((item) => item.offer.guests === 2);
+//       reRenderMarkers(filteredGuests);
+//     } else {
+//       reRenderMarkers(offers);
+//     }
+//   })
+// }
+
+
 
 export {
   addFilterListener
